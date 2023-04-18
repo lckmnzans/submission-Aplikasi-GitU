9e64a2aece077357b7ddc05d2a479406925fa1a5
lckmnzans <lckmnzans@gmail.com>
2023-04-18 22:34:25 +0700

    creating detailActivity and implementing onClickListener: done

--- app/src/main/java/com/dicoding/gitu/GithubResponse.kt
+++ app/src/main/java/com/dicoding/gitu/GithubResponse.kt
@@ -15,15 +15,8 @@ data class GithubResponse(
 )
 
 data class ItemsItem(
-
-	@field:SerializedName("following_url")
-	val followingUrl: String? = null,
-
 	@field:SerializedName("login")
 	val login: String? = null,
-
-	@field:SerializedName("followers_url")
-	val followersUrl: String? = null,
 
 	@field:SerializedName("url")
 	val url: String? = null,
@@ -32,5 +25,5 @@ data class ItemsItem(
 	val avatarUrl: String? = null,
 
 	@field:SerializedName("id")
-	val id: Int? = null,
+	val id: Int? = null
 )
