b615a797787842150d54ec179ade4e63d43ef0ba
lckmnzans <lckmnzans@gmail.com>
2023-04-18 12:11:01 +0700

    creating searchbar and implement searching: done

--- app/src/main/java/com/dicoding/gitu/MainActivity.kt
+++ app/src/main/java/com/dicoding/gitu/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.dicoding.gitu
 
+import android.app.SearchManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dicoding.gitu.api.ApiConfig
 import com.dicoding.gitu.databinding.ActivityMainBinding
@@ -18,7 +22,7 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val TAG = "MainActivity"
-        private const val QUERY = "fadhil"
+        private var QUERY = "fadhil"
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,34 +31,72 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar?.title = "Github User"
 
+        //menerapkan recyclerV dalam linear vertikal
         activityMainBinding.rvUsers.layoutManager = LinearLayoutManager(this)
         findUser()
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.option_search, menu)
+
+        //mulai implementasi search bar
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search_hint)
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                searchView.clearFocus()
+                QUERY = query
+                findUser()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+        //akhir implementasi seach bar
+        return true
+    }
+
     private fun showLoading(isLoading: Boolean) {
         if (isLoading) {
+            //jika sedang loading, progress bar muncul dan recyclerview tidak muncul
             activityMainBinding.progressBar.visibility = View.VISIBLE
+            activityMainBinding.rvUsers.visibility = View.GONE
         } else {
+            //jika sedang tidak loading, progress bar tidak muncul tetapi recyclerview muncul
             activityMainBinding.progressBar.visibility = View.GONE
+            activityMainBinding.rvUsers.visibility = View.VISIBLE
         }
     }
     private fun findUser() {
         showLoading(true)
         val client = ApiConfig.getApiService().getUser(QUERY)
+        //menerapkan kembalian dari Http Request ke dalam Callback dengan tipe parameter GithubResponse
         client.enqueue(object: Callback<GithubResponse> {
+            //jika didapatkan response
             override fun onResponse(
                 call: Call<GithubResponse>,
                 response: Response<GithubResponse>
             ) {
                 showLoading(false)
                 if (response.isSuccessful) {
+                    //mendapatkan response sukses dari Http Request
                     val responseBody = response.body()
                     if (responseBody != null) {
+                        //jika body response tidak null, diambil data dari key items dan totalCount
                         setUserData(responseBody.items, responseBody.totalCount)
                     }
                 } else {
+                    //medapatkan response tidak sukses dari Http Request
                     Log.e(TAG, "onFailure: ${response.message()}")
                 }
             }
+            //jika tidak didapatkan respon
             override fun onFailure(call: Call<GithubResponse>, t: Throwable) {
                 showLoading(false)
                 Log.e(TAG, "onFailure: ${t.message}")
@@ -63,13 +105,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setUserData(users: List<ItemsItem>, count: Int) {
+        //membuat list kosong dengan tipe parameter User -> dari kelas data User
         val list = ArrayList<User>()
         for (user in users) {
+            //untuk tiap-tiap user, data-datanya dimasukkan ke dalam kelas data User() yang akan dikirimkan ke adapter
             val userData = User(user.avatarUrl.toString(), user.login.toString(), user.followersUrl.toString(), user.followingUrl.toString())
+            //menambahkan data tersebut ke dalam list
             list.add(userData)
         }
+        //menerapkan list ke dalam UserAdapter
         val listUser = UserAdapter(list)
+        //menerapkan adapter tersebut ke adapter dlm MainActivity
         activityMainBinding.rvUsers.adapter = listUser
-        activityMainBinding.tvItemCount.text = count.toString()
     }
 }
